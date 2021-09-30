8397c516bfb15f5b20b8ef576688c72ddac13c8f
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2021-09-30 13:08:38 -0700

    Add: 5-printf.c file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: prints sentence
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}

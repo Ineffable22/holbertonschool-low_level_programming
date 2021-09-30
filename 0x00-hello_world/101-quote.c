b21e84288ea29c089161da2a43388fa37850f1f3
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2021-09-30 16:09:08 -0700

    Add: 101-quote.c file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+# define TEXT printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n")
+
+int main(void)
+{
+	TEXT;
+	return (0);
+}

9ac929cf27370a23c98109680485efb3763aab18
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2021-10-05 07:55:41 -0700

    Add: 0-putchar.c file

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints the values
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	putchar(95);
+	putchar(112);
+	putchar(117);
+	putchar(116);
+	putchar(99);
+	putchar(104);
+	putchar(97);
+	putchar(114);
+	putchar(10);
+	return (0);
+}

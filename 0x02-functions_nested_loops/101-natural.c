9d03367c0c36e0c247b718e05431278c0d673249
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2021-10-06 07:26:24 -0700

    code optimized

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -5,26 +5,21 @@
  * of 3 or 5 below 1024
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	unsigned long int sum3, sum5, sum;
-	int i;
+	int n, m;
 
-	sum3 = 0;
-	sum5 = 0;
-	sum = 0;
-
-	for (i = 0; i < 1024; ++i)
+	n = 0;
+	m = 0;
+	while (n < 1024)
 	{
-		if ((i % 3) == 0)
+		if (n % 3 == 0 || n % 5 == 0)
 		{
-			sum3 = sum3 + i;
-		} else if ((i % 5) == 0)
-		{
-			sum5 = sum5 + i;
+			m = m + n;
 		}
+		n++;
 	}
-	sum = sum3 + sum5;
-	printf("%lu\n", sum);
+	printf("la suma total es: %d\n", m);
 	return (0);
 }

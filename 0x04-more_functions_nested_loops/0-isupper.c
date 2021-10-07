c616e193ec787759cf870982cd5d39f0f2401bc4
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2021-10-07 08:03:44 -0700

    Add: 0-isupper.c file

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: character to be checked
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	return (c >= 65 && c <= 90);
+}

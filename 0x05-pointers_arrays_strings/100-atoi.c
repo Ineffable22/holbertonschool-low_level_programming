921adcf09bbfca4a091e2d3973f3ea72d0102b20
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2021-10-12 20:54:55 -0700

    Add: 100-atoi.c file

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
+ */
+int _atoi(char *s)
+{
+	int i, d, n, x, f, digit;
+
+	i = 0;
+	d = 0;
+	n = 0;
+	x = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[x] != 0)
+		x++;
+
+	while (i < x && f == 0)
+	{
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
+	}
+
+	if (f == 0)
+		return (0);
+
+	return (n);
+}

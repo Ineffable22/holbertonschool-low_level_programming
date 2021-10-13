2b2ccb5106be3c481c71a3e2784284f24f4b4dda
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2021-10-12 20:22:18 -0700

    9-strcpy.c update: reworked for of dest

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -18,9 +18,8 @@ char *_strcpy(char *dest, char *src)
 	{
 		n++;
 	}
-	for (i = 0; i < n && src[i] != 0; i++)
+	for (i = 0; i < n; i++)
 		dest[i] = src[i];
-	for ( ; i < n; i++)
-		dest[i] = 0;
+	dest[i] = 0;
 	return (dest);
 }

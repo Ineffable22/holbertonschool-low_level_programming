1cba63da96d12af7eed1c408a76588e394f71525
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2021-10-20 20:17:05 -0700

    added description

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,5 +1,11 @@
 #include "main.h"
-
+/**
+ * wildcmp - Compare two strings allowing for wildcard char
+ * @s1: String being compared
+ * @s2: String being compared against
+ *
+ * Return: 1 if considered identical, 0 otherwise
+ */
 int wildcmp(char *s1, char *s2)
 {
 	if (*s1 == '\0' && *s2 == '\0')

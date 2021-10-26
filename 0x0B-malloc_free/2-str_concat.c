8859efb608bc282911a43941a6b6c6bb85cfcb33
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2021-10-26 14:14:37 -0700

    2-str_concat.c update: The right amount of memory was allocated and corrected space

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,7 +13,7 @@ char *str_concat(char *s1, char *s2)
 	int i, j, len1, len2;
 	char *p;
 
-	if (*s1 == 0 || *s2 == 0)
+	if (s1 == NULL || s2 == NULL)
 		return (NULL);
 
 	for (len1 = 0; *(s1 + len1) != 0; len1++)
@@ -21,14 +21,18 @@ char *str_concat(char *s1, char *s2)
 	for (len2 = 0; *(s2 + len2) != 0; len2++)
 	{}
 
-	p = malloc(sizeof(char) * len1 * len2);
+	p = malloc(sizeof(char) * (len1 + len2 + 1));
+
+	if (p == NULL)
+		return (NULL);
+
 	for (i = 0; i < len1; i++)
-	{
 		*(p + i) = *(s1 + i);
-	}
+
 	for (j = 0; j < len2; j++)
-	{
 		*(p + i + j) = *(s2 + j);
-	}
+
+	*(p + i + j) = 0;
+
 	return (p);
 }

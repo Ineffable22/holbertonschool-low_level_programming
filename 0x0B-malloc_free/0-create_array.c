4d4371609d792bea0d3d8377fad621a69e805f17
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2021-10-26 13:37:52 -0700

    0-create_array.c update adds (int *) in malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,7 +16,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 
-	p = malloc(sizeof(char) * size);
+	p = (char *)malloc(sizeof(char) * size);
 
 	for (i = 0; i < size; i++)
 		*(p + i) = c;

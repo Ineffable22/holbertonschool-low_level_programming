e6c9298254be1674ef5436603ebd45734f541aec
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2021-10-26 14:32:36 -0700

    2-str_concat.c update: new restriction

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,9 +13,6 @@ char *str_concat(char *s1, char *s2)
 	int i, j, len1, len2;
 	char *p;
 
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
-
 	for (len1 = 0; *(s1 + len1) != 0; len1++)
 	{}
 	for (len2 = 0; *(s2 + len2) != 0; len2++)
@@ -29,10 +26,10 @@ char *str_concat(char *s1, char *s2)
 	for (i = 0; i < len1; i++)
 		*(p + i) = *(s1 + i);
 
-	for (j = 0; j < len2; j++)
-		*(p + i + j) = *(s2 + j);
+	for (j = i, i = 0; j < len2; j++, i++)
+		*(p + j) = *(s2 + i);
 
-	*(p + i + j) = 0;
+	*(p + j) = 0;
 
 	return (p);
 }

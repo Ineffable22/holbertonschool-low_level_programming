b7d6950f81a5d3db0a74a67b1a6398153da998df
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2021-10-28 14:37:30 -0700

    2-calloc.c update

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,12 +9,12 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	p = malloc(size * nmemb);
+	p = malloc(nmemb * size);
 
 	if (p == NULL)
 		return (NULL);
 
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < (nmemb * size); i++)
 	{
 		*(p + i) = 0;
 	}

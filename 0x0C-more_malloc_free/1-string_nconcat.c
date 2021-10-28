5acaa6eb3deeebe8086f2c2045a05c1dfbfd3bbc
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2021-10-28 13:18:10 -0700

    1-string_nconcat.c update: new restriction was changed

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -21,9 +21,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n >= len2)
 		n = len2;
 
+	
 	p = malloc(sizeof(char) * (len1 + n + 1));
 
-	if (p == NULL)
+	if (p == 0)
 		return (NULL);
 
 	for (i = 0; i < len1; i++)

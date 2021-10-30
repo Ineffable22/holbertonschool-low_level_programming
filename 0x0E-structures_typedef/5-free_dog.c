534d8bb68ef5f149eefa452e15465ba505452bea
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2021-10-30 16:06:31 -0700

    Add: tasks

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - frees memory allocated for a struct dog
+ * @d: struct dog to free
+ */
+void free_dog(dog_t *d)
+{
+	if(d == NULL)
+		return;
+	else
+	{
+		free(d->owner);
+		free(d->name);
+		free(d);
+	}
+}
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,27 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+typedef struct dog dog_t;
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+
+#endif

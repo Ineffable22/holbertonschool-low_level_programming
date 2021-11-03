8e49f9944181d9830334627963fff13519e2a495
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2021-11-03 21:37:51 +0000

    Add: Task 3 files

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,30 @@
+#include "3-calc.h"
+/**
+ * main - program that perfroms simple operations
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i, j, res;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	i = atoi(argv[1]);
+	j = atoi(argv[3]);
+
+	if (*(argv[2] + 1) != 32 || *(argv[2] + 1) != 39)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	res = (*get_op_func(argv[2]))(i, j);
+	printf("%d\n", res);
+	return (0);
+}

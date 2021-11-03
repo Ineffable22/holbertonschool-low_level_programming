9a491113c9286b41469a267b1be9f74e13bc5399
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2021-11-03 18:38:39 +0000

    Add: function_pointers.h 0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+ * print_name - prints a name
+ * @name: name to print
+ * @f: pointer to the printing function
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (*f || *name)
+		(*f)(name);
+}

e2271fd7cd4d9dc0f5ef22fb240d3cfeb3dd02a1
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2021-11-04 21:19:43 +0000

    Add: 100-hello_world.asm file

--- 0x10-variadic_functions/100-hello_world.asm
+++ 0x10-variadic_functions/100-hello_world.asm
@@ -1,17 +1,17 @@
-section	.text
 	global main
+section	.text
 
 main:
-	mov	eax,0x4
-	mov	ebx,1
-	mov	ecx,message
-	mov	edx,message_length
-	int	0x80
+	mov eax,0x4
+	mov ebx,1
+	mov ecx,message
+	mov edx,message_length
+	int 0x80
 
-	mov	eax,0x1
-	mov	ebx,0
-	int	0x80
+	mov eax,0x1
+	mov ebx,0
+	int 0x80
 
-section		.data
-message			db	"Hello World!",0xa
-message_length		equ	$-message
+section	.data
+message	db "Hello World!",0xA
+message_length equ $-message

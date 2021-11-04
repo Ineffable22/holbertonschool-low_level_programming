de311da82aac822f829ad32e8e1c04a956c2b509
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2021-11-04 19:19:22 +0000

    New restriction

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -9,14 +9,20 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
 	va_list a;
+	char *s;
 
 	va_start(a, n);
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%s", va_arg(a, char *));
+		s = va_arg(a, char *);
+		if (s == NULL)
+			printf("(nil)");
+		else
+			printf("%s", s);
 		if (i + 1 != n && separator)
 			printf("%s", separator);
 	}
 	putchar(10);
+	va_end(a);
 }

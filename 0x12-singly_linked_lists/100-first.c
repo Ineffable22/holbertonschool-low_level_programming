5fc555baaebeae67eaf8eb63c6203088e493d364
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2021-11-11 17:07:01 -0500

    100-first.c update: other pointer was added

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,10 +7,15 @@ void first(void) __attribute__ ((constructor));
 void first(void)
 {
 	int i;
-	char *c = "You're beat! and yet, you must allow,\nI bore my house upon my back!\n";
+	char *c = "You're beat! and yet, you must allow,\n";
+	char *h = "I bore my house upon my back!\n";
 
-	for(i = 0; *(c + i); i++)
-        {
+	for (i = 0; *(c + i); i++)
+	{
 		write(1, &(*(c + i)), 1);
-        }
+	}
+	for (i = 0; *(h + i); i++)
+	{
+		write(1, &(*(h + i)), 1);
+	}
 }

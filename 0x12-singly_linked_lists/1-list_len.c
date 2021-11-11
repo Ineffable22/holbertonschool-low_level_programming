0196d5b52293612ea66830ca5c2b79ca4db9cdf9
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2021-11-11 13:27:35 -0500

    Add: 1-list_len.c file and lists.h(new prototype)

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * list_len - Counts all the elements in a linked list_t list.
+ * @h: Pointer to struct
+ *
+ * Return: The number of elements in a linked list_t list.
+ */
+size_t list_len(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}

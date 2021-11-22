8cb67c8c8f8abe9529b465bba957ca8b540415b6
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2021-11-22 15:45:53 -0500

    Add: 0-read_textfile.c main.h

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,46 @@
+#include "main.h"
+/**
+ * read_textfile - Reads a text file and prints
+ * it to the POSIX standard output.
+ * @filename: Is argv of the file Requiescat
+ * @letters: Size of the text to print
+ *
+ * Return: The actual number of letters it could read and print
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *text;
+	int fh;
+	ssize_t i;
+
+	text = malloc(letters);
+	if (text == NULL)
+		return (0);
+
+	fh = open(filename, O_RDONLY);
+	if (fh == -1)
+	{
+		free(text);
+		close(fh);
+		return (0);
+	}
+
+	i = read(fh, text, letters);
+	if (i == -1)
+	{
+		free(text);
+		close(fh);
+		return (0);
+	}
+
+	if (write(1, text, i) == -1)
+	{
+		free(text);
+		close(fh);
+		return (0);
+	}
+
+	close(fh);
+	free(text);
+	return (i);
+}

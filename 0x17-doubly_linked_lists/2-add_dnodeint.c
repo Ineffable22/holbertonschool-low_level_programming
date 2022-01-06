cb3c9f7116d61eeb7ea7aa474ef8a7b7155184f0
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-01-06 13:11:43 -0500

    2-add_dnodeint.c update: new restriction was added

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -22,6 +22,10 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	node->n = n;
 	node->next = *head;
 	node->prev = NULL;
+
+	if (*head)
+		(*head)->prev = node;
+
 	*head = node;
 	return (node);
 }

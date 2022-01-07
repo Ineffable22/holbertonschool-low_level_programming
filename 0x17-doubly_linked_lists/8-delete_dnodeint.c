9b956783d1f6fdf778c8434ced05aade7aa763f2
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-01-06 19:26:57 -0500

    8-delete_dnodeint.c update: new prototype and restriction was added

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,6 +1,18 @@
 #include "lists.h"
 
 /**
+ * dlistint_len - Computes the elements of a dlistint_t list.
+ * @h: Pointer to struct.
+ *
+ * Return: The number of nodes.
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	if (h)
+		return (1 + dlistint_len(h->next));
+	return (0);
+}
+/**
  * delete_dnodeint_at_index - Deletes node of a dlistint_t linked list.
  * @head: Double pointer to struct.
  * @index: Index position to remove node.
@@ -9,13 +21,12 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *tmp = NULL;
+	dlistint_t *tmp = *head;
 
-	if (*head == NULL)
+	if (*head == NULL || dlistint_len(tmp) < index + 1)
 		return (-1);
 	if (index > 0 && *head != NULL)
 		return (delete_dnodeint_at_index(&((*head)->next), index - 1));
-	tmp = *head;
 	if (index > 0 && *head == NULL)
 		return (-1);
 	if (index == 0)

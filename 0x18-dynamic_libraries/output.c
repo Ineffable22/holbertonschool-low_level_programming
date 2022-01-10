fe541d9364ee9cee56a271beaadb95ed17eca04b
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-01-10 18:58:43 -0500

    update: flag was changed

--- 0x18-dynamic_libraries/output.c
+++ 0x18-dynamic_libraries/output.c
@@ -2,10 +2,10 @@
 
 int printf(const char *format, ...)
 {
-	return (0);
+        return (0);
 }
 
 int puts (const char *s)
 {
-	return (write(1, "9 8 10 24 75 - 9\nCongratulations, you win the Jackpot!\n", 57));
+        return (write(1, "9 8 10 24 75 - 9\nCongratulations, you win the Jackpot!\n", 57));
 }

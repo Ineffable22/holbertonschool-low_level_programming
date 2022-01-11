691155b9f6861504ccca355fde0bb579f3bd8fdc
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-01-10 20:01:48 -0500

    code was reworked

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/liboutput.so https://github.com/Ineffable22/holbertonschool-low_level_programming/tree/main/0x18-dynamic_libraries/liboutput.so
+wget -P /tmp https://github.com/Ineffable22/holbertonschool-low_level_programming/tree/main/0x18-dynamic_libraries/liboutput.so
 export LD_PRELOAD=/tmp/liboutput.so
---
+++ 0x18-dynamic_libraries/liboutput.c
@@ -0,0 +1,21 @@
+#include <string.h>
+
+int rand()
+{
+	static int num = -1;
+
+	num++;
+	if (num == 0)
+		return 8;
+	if (num == 1)
+		return 8;
+	if (num == 2)
+		return 7;
+	if (num == 3)
+		return 9;
+	if (num == 4)
+		return 23;
+	if (num == 5)
+		return 74;
+	return (num * num % 30000);
+}

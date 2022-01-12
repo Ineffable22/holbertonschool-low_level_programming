ceb5cf5a67e2dd7fe3dd502f248411507793ffa7
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-01-11 20:30:03 -0500

    Code was reworked

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/libout_put.so https://raw.github.com/Ineffable22/holbertonschool-low_level_programming/main/0x18-dynamic_libraries/liboutput.so
-export LD_PRELOAD=/tmp/libout_put.so
+wget -O /tmp/liboutput.so https://raw.github.com/Ineffable22/holbertonschool-low_level_programming/main/0x18-dynamic_libraries/liboutput.so
+export LD_PRELOAD=/tmp/liboutput.so

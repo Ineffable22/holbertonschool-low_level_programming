7252f5dede9784e882a20cd87051c30e31aab676
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-01-28 12:36:17 -0500

    hash_tables.h update: order was modified

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <stdio.h>
 
+/* -- struct C -- */
 /**
  * struct hash_node_s - Node of a hash table
  *
@@ -34,16 +35,7 @@ typedef struct hash_table_s
 	hash_node_t **array;
 } hash_table_t;
 
-hash_table_t *hash_table_create(unsigned long int size);
-unsigned long int hash_djb2(const unsigned char *str);
-unsigned long int key_index(const unsigned char *key, unsigned long int size);
-unsigned long int key_index(const unsigned char *key, unsigned long int size);
-int hash_table_set(hash_table_t *ht, const char *key, const char *value);
-char *hash_table_get(const hash_table_t *ht, const char *key);
-void hash_table_print(const hash_table_t *ht);
-void hash_table_delete(hash_table_t *ht);
-
-
+/* -- struct PHP -- */
 /**
  * struct shash_node_s - Node of a sorted hash table
  *
@@ -63,6 +55,7 @@ typedef struct shash_node_s
 	struct shash_node_s *snext;
 } shash_node_t;
 
+
 /**
  * struct shash_table_s - Sorted hash table data structure
  *
@@ -81,9 +74,23 @@ typedef struct shash_table_s
 	shash_node_t *stail;
 } shash_table_t;
 
+/* - C - */
+
+hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+void set_check(hash_node_t **head, hash_node_t **node);
+char *hash_table_get(const hash_table_t *ht, const char *key);
+void hash_table_print(const hash_table_t *ht);
+void hash_table_delete(hash_table_t *ht);
+
+/* - PHP - */
+
 shash_table_t *shash_table_create(unsigned long int size);
 int shash_table_set(shash_table_t *ht, const char *key, const char *value);
-void set_check(hash_node_t **head, hash_node_t **node);
+void sset_check(shash_node_t **head, shash_node_t **node);
 char *shash_table_get(const shash_table_t *ht, const char *key);
 void shash_table_print(const shash_table_t *ht);
 void shash_table_print_rev(const shash_table_t *ht);

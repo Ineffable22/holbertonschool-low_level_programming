2b17b2dc1be27f604eaad484d93e4f75149a1e90
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-01-28 11:34:16 -0500

    3-hash_table_set.c update: cast was added in key

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -25,7 +25,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		node->next = ht->array[index];
 	}
 	ht->array[index] = node;
-	node->key = key;
+	node->key = (char *)key;
 	node->value = strdup(value);
 	node->next = NULL;
 

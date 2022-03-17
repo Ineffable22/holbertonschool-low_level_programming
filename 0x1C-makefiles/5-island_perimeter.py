88e07d9cd96ad5ae1f9d5d1e2426cfe613a09a8c
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-03-17 15:43:01 -0500

    Update(05-island_perimeter.py): Try was changed per if

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -10,28 +10,17 @@ def island_perimeter(grid):
         max_row = len(grid[row]) > 100 \
             if len(grid[row]) == 100 else len(grid[row])
         for col in range(max_row):
-            try:
-                top = grid[row - 1][col]
-            except:
-                top = 1
-            try:
-                bot = grid[row + 1][col]
-            except:
-                bot = 1
-            try:
-                left = grid[row][col - 1]
-            except:
-                left = 1
-            try:
-                right = grid[row][col + 1]
-            except:
-                right = 1
-            if top == 0 and grid[row][col] == 1:
-                count += 1
-            if bot == 0 and grid[row][col] == 1:
-                count += 1
-            if left == 0 and grid[row][col] == 1:
-                count += 1
-            if right == 0 and grid[row][col] == 1:
-                count += 1
+            if grid[row][col] == 1:
+                top = grid[row - 1][col] if row - 1 != -1 else 0
+                bot = grid[row + 1][col] if row + 1 != max_grid else 0
+                left = grid[row][col - 1] if col - 1 != -1 else 0
+                right = grid[row][col + 1] if col + 1 != max_row else 0
+                if top == 0:
+                    count += 1
+                if bot == 0:
+                    count += 1
+                if left == 0:
+                    count += 1
+                if right == 0:
+                    count += 1
     return count

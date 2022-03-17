94a6fda6672e59278597fcbefb7ea8e8f93a281b
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-03-17 14:37:45 -0500

    Add: task 5-island_perimeter.py

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" Island Perimeter """
+
+
+def island_perimeter(grid):
+    """ Returns the perimeter of the island described in grid """
+    count = 0
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            try:
+                top = grid[row - 1][col]
+            except:
+                top = 0
+            try:
+                bot = grid[row + 1][col]
+            except:
+                bot = 0
+            try:
+                left = grid[row][col - 1]
+            except:
+                left = 0
+            try:
+                right = grid[row][col + 1]
+            except:
+                right = 0
+            if top == 0 and grid[row][col] == 1:
+                count += 1
+            if bot == 0 and grid[row][col] == 1:
+                count += 1
+            if left == 0 and grid[row][col] == 1:
+                count += 1
+            if right == 0 and grid[row][col] == 1:
+                count += 1
+    return count

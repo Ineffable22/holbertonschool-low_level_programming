09f619de93b057a71b9e323a5c58ea6a636929b6
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-05-04 20:59:19 -0500

    Feat(search_algos.h): new prototype

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -8,4 +8,7 @@
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 
+/*---------- advanced ----------*/
+int jump_search(int *array, size_t size, int value);
+
 #endif

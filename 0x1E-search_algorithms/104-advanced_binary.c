322a4c7173d92d95851f46a3bfab62d8ef0a3a3f
Ineffable22 <miguel_grillo_orellana@hotmail.com>
2022-05-07 22:07:20 -0500

    Feat(104-advanced_binary.c): recursion was added

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -26,6 +26,38 @@ void printArray(int *array, size_t start, size_t end)
 }
 
 /**
+ * binary_search_v3 - searches for a value in an array of integers using
+ * the Linear search algorithm with recursion
+ * @array: pointer to the first element of the array
+ * @value: value to search for
+ * @start: index to start
+ * @end: index to end
+ *
+ * Return: the first index where value is located
+ */
+int binary_search_v3(int *array, int value, size_t start, size_t end)
+{
+	size_t mid = 0;
+
+	if (start < end)
+	{
+		mid = (start + end) / 2;
+		printArray(array, start, end);
+		if (value == array[mid] &&
+		    (array[mid - 1] != value))
+			return (mid);
+		else if (value <= array[mid])
+			return (binary_search_v3(array, value, start, mid));
+		else if  (value > array[mid])
+			return (binary_search_v3(array, value, mid + 1, end));
+	}
+	printArray(array, start, end);
+	if (value == array[end])
+		return (end);
+	return (-1);
+}
+
+/**
  * advanced_binary - searches for a value in an array of integers using
  * the Corrected Linear search algorithm corrected
  * @array: pointer to the first element of the array
@@ -36,25 +68,7 @@ void printArray(int *array, size_t start, size_t end)
  */
 int advanced_binary(int *array, size_t size, int value)
 {
-	size_t start = 0, end = size - 1, mid = 0;
-
 	if (array && size > 0)
-	{
-		while (start < end)
-		{
-			mid = (start + end) / 2;
-			printArray(array, start, end);
-			if (value == array[mid] &&
-			    (array[mid - 1] != value))
-				return (mid);
-			else if (value <= array[mid])
-				end = mid;
-			else if  (value > array[mid])
-				start = mid + 1;
-		}
-		printArray(array, start, end);
-		if (value == array[end])
-			return (end);
-	}
+		return (binary_search_v3(array, value, 0, size - 1));
 	return (-1);
 }
